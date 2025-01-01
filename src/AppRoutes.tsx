f33737f65346b0c6c8aa2a49504687f32b415d2a
Rohit Mittal <rohit.mittal@salescode.ai>
2025-01-01 22:29:35 +0530

    added searchBar

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -5,11 +5,11 @@ import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
+import SearchPage from "./pages/SearchPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-
       <Route
         path="/"
         element={
@@ -20,6 +20,14 @@ const AppRoutes = () => {
       />
 
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      <Route
+        path="/search/:city"
+        element={
+          <Layout showHero={false}>
+            <SearchPage />
+          </Layout>
+        }
+      />
 
       <Route element={<ProtectedRoute />}>
         <Route
@@ -31,7 +39,7 @@ const AppRoutes = () => {
           }
         />
 
-<Route
+        <Route
           path="/manage-restaurant"
           element={
             <Layout>
@@ -39,11 +47,9 @@ const AppRoutes = () => {
             </Layout>
           }
         />
-
       </Route>
 
       <Route path="*" element={<Navigate to="/" />} />
-      
     </Routes>
   );
 };
---
+++ src/api/RestaurantApi.tsx
@@ -0,0 +1,35 @@
+import { SearchState } from "@/pages/SearchPage";
+import { RestaurantSearchResult } from "@/types";
+import { useQuery } from "react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useSearchRestaurant = (
+  searchState: SearchState,
+  city?: string
+) => {
+  const createSearchRequest = async (): Promise<RestaurantSearchResult> => {
+    const params = new URLSearchParams();
+    params.set("searchQuery", searchState.searchQuery);
+    const response = await fetch(
+      `${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to get restaurant");
+    }
+
+    return response.json();
+  };
+
+  const { data: results, isLoading } = useQuery(
+    ["searchRestaurants", searchState],
+    createSearchRequest,
+    { enabled: !!city }
+  );
+
+  return {
+    results,
+    isLoading,
+  };
+};
